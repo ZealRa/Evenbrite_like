b50c5a4a3e7b5070d9bdba1e30719752b792249a
thomas.bobichon@hotmail.fr <thomas.bobichon@hotmail.fr>
2024-05-07 17:05:37 +0200

    Fin de journée

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,7 +2,32 @@ class EventsController < ApplicationController
   def index
   end
 
+  def to_param
+    "#{id}-#{title.parameterize}"
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to @event
+    else
+      render :new
+    end
+  end
+
+
   def show
     @event = Event.find(params[:id])
   end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :start_date, :duration, :description, :price, :location)
+  end
+
 end
